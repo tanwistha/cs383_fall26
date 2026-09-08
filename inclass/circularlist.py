feb723ef3dde57151e81d09faedf26b7499ca27b
Tanwistha Saha <tanwi82@gmail.com>
2026-09-08 11:39:10 -0700

    soln to circularlist

--- inclass/circularlist.py
+++ inclass/circularlist.py
@@ -11,18 +11,34 @@ class CircularList:
 
     def insert(self, data):
         # Append a new node with data to the end of the circular linked list
-        # TODO
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = self.head
+        else:
+            current = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = new_node
+            new_node.next = self.head
      
     def traverse(self):
         # Traverse and display the elements of the circular linked list
-        # TODO
+        if not self.head:
+            return
+        current = self.head
+        while True:
+            print(current.data)
+            current = current.next
+            if current == self.head:
+                break
 
-# l = CircularList()
-# print("insert node..")
-# l.insert(1)
-# l.insert(2)
-# l.insert(3)
-# l.traverse()
-# print("inserting again..")
-# l.insert(4)
-# l.traverse()
+l = CircularList()
+print("insert node..")
+l.insert(1)
+l.insert(2)
+l.insert(3)
+l.traverse()
+print("inserting again..")
+l.insert(4)
+l.traverse()
